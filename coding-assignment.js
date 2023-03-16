451f1e351f224cd3ffbb209af9f49778c5457328
Jorge Rivera <junkiedorge@gmail.com>
2023-03-16 01:00:09 -0700

    updated code

--- coding-assignment.js
+++ coding-assignment.js
@@ -3,35 +3,71 @@
 //All questions should be printed to your Browser’s console using console.log()//
 
 //1.  Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.//
-//1a.Programmatically subtract the value of the first element in the array from the value in the last element of the array. //  
-//1b. Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).//
-//1c. Use a loop to iterate through the array and calculate the average age. //
+
+console.log("\nQ1:")
 let ages = [3, 9, 23, 64, 2, 8, 28, 93];
- console.log("Q1A:",ages[ages.length -1] - ages[0]);
- ages.push(22)
- console.log("Q1BCA:", ages[ages.length -1] - ages[0]);
+
+//1a.Programmatically subtract the value of the first element in the array from the value in the last element of the array. //  
+
+console.log("Q1A:",ages[ages.length -1] - ages[0]);
+
+//1b. Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).//
+
+ages.push(22)
+ console.log("Q1B:", ages[ages.length -1] - ages[0]);
+
+//1c. Use a loop to iterate through the array and calculate the average age. //
+
+let sum = 0;
+for (let i=0; i< ages.length; i++){
+    sum += ages[i];
+    console.log(sum)
+}
+
+ 
+ 
+ 
+
+
 
 //2.  Create an array called names that contains the following values: ‘Sam’, ‘Tommy’, ‘Tim’, ‘Sally’, ‘Buck’, ‘Bob’.//
+console.log("\nQ2:")
+let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
 //2a. Use a loop to iterate through the array and calculate the average number of letters per name. //
+let chars = 0;
+
+for (let i =0; i<names.length; i++){
+    console.log("name:",names[i]);
+    console.log("length of name:",names[i].length);
+    chars+=names[i].length;
+
+    console.log(chars)
+}
+
+console.log("average:", chars / names.length);
+
+   //names =['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
+   //let lengths = names.map(function(element){
+    //return element.length;
+//});
+    //console.log(lengths);
+    //let sum = lengths.reduce(function(accummulator, currentValue){
+   //     return accummulator + currentValue;
+   // });
+    //console.log("Q2A:", sum);
+    //this code worked only to get the numbers right but did not add as they went along just adds at the final//
+     
 //2b. Use a loop to iterate through the array again and concatenate all the names together, separated by spaces. //
 
-   namesArray =['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
-   let lengths = namesArray.map(function(element){
-    return element.length;
-});
-    console.log(lengths);
-    let sum = lengths.reduce(function(accummulator, currentValue){
-        return accummulator + currentValue;
-    });
-    console.log("Q2A:", sum);
-     
+
     
 
 //3.  How do you access the last element of any array?//
-console.log("Q3A: namesArray.length-1");
+console.log("Q3A: array(namesArray.length-1)");  //always name array length gives you the full array and the minus 1 gives the end of the array. 
 
 //4.  How do you access the first element of any array?//
-console.log("Q4A: namesArray[0]");
+console.log("Q4A: namesArray[0]"); // while names array in [0] names the postition of the index it begins at. 
+
 //5.  Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.//
 
 //For example://
@@ -39,17 +75,32 @@ console.log("Q4A: namesArray[0]");
 //let names = ["Kelly", "Sam", "Kate"]; //   // starting with this array
 //let nameLengths = [5, 3, 4]; //            // create a new array//
 
+let nameLengths = [];
+for (let i=0; i<nameLengths.length; i++){
+    console.log(names[i].length)
+    nameLengths.push(name[i].length);
+    console.log(nameLengths);
+}
+console.log("\nQ5A: name lenght array:", nameLengths);
+
 //6.  Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. //
 
-
+let sums= 0
+for (let i=0; i< nameLengths.length; i++){
+    sums += nameLengths[i];
+   
+}
+console.log("Q6A:",sum)
 //7.  Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’).//
-function twoParameters(word, n);
+
+
+
 
 //8.  Write a function that takes two parameters, firstName and lastName, and returns a full name.  The full name should be the first and the last name separated by a space.//
 function createFullName(firstName, lastName){
    return firstName + ' ' + lastName;
 }
-let fullName = createFullName('Jorge', 'Rivera'); //can also be used as console.log(createFullName('Jorge', 'Rivera')\\
+fullName = createFullName('Jorge', 'Rivera'); //can also be used as console.log(createFullName('Jorge', 'Rivera')\\
 console.log("Q8A:",fullName)
 
 
@@ -78,14 +129,30 @@ function findAverage (arrayOfNumbers){
     console.log("Q10A:", findAverage (scores));
 
 //11.  Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.//
+console.log("\nQ11A")
 function largerAverage(array1,array2) {
-if (findAverage(array1) > findAverage(array2)) {
-    return true;
-} else{
-    return false;
+    let sum1= 0;
+    let sum2= 0;
+
+    for(let i=0; i < array1.length; i++){
+    sum1 += array1[i];
+    console.log("sum1",sum1);
+    }
+    for(let i=0; i < array2.length; i++){
+    sum2 += array2[i];
+    console.log("Sum2",sum2);
+    }
+    let average1 = sum1 / array1.length;
+    let average2 = sum2 / array2.length;
+    
+    if(average1> average2){
+        console.log(true)
+    }
+    else{
+        console.log(false)
+    }
 }
-}
-console.log("Q11A:", largerAverage(scores, arrayOfNumbers.length));
+largerAverage([10,20,30,40,50], [10,20,30,40,60]);
 
 
 
